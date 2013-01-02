a985d67aa434f2cbaca44ec219fa54daa2b5fa31
Steven Mohr <business@stevenmohr.de>
2013-01-02 09:19:19 -0800

    added simple view and simple template

--- CrimeMonitor/urls.py
+++ CrimeMonitor/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+from crime_main.views import MainView
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,4 +15,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', view=MainView.as_view(), name='home'),
 )
--- crime_main/views.py
+++ crime_main/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from django.views.generic import ListView
+from django.views.generic.base import TemplateView
+from crime_main.models import Crime
+
+class MainView(ListView):
+    template_name = "main.html"
+    queryset = Crime.objects.all()
+    context_object_name = "crimes"
