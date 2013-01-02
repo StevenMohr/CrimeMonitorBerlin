3ce58d94d591d8c946e13179ad43b042b6753a96
Steven Mohr <business@stevenmohr.de>
2013-01-02 09:27:25 -0800

    fixed fixtures (duplicate primary key) extended fab file

---
+++ crime_main/sql/district_.sql
@@ -0,0 +1,2 @@
+/* Postgres/PostGIS-only */
+ UPDATE ONLY crime_main_district SET way=ST_Transform(subquery.way, 4326), name=subquery.name, area_id = subquery.osm_id FROM (SELECT planet_osm_line.way, planet_osm_line.osm_id, crime_main_district.name from planet_osm_line INNER JOIN crime_main_district on (planet_osm_line.osm_id = crime_main_district.area_id)) AS subquery WHERE crime_main_district.area_id = subquery.osm_id;
--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,4 @@
+from fabric.context_managers import lcd
 from fabric.operations import sudo, local
 
 __author__ = 'steven'
@@ -6,3 +7,8 @@ __author__ = 'steven'
 def install_system_local():
     local('sudo su -c "createdb -T template_postgis crimemonitor" postgres')
     local('sudo su -c "psql -f init_db.sql" postgres')
+    local('python manage.py syncdb')
+    with lcd('/tmp/'):
+        local('wget http://download.geofabrik.de/openstreetmap/europe/germany/berlin.osm.bz2')
+        local('osm2pgsql -d crimemonitor -U crime -W -s berlin.osm.bz2')
+    local('sudo su -c "psql -f crime_main/sql/district_.sql  crimemonitor" postgres')
