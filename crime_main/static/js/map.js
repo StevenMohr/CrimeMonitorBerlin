7b50db0d8c19c3e77043bc93217844e46231ff1f
Max <max@max-laptop.(none)>
2013-01-28 13:31:06 +0100

    Changed the Colors in certain steps from green to dark red

--- crime_main/static/js/map.js
+++ crime_main/static/js/map.js
@@ -9,12 +9,29 @@ in_options = {
 to_district_vector = function(wkt, ratio) {
   var feature, format;
   format = new OpenLayers.Format.WKT(in_options);
+
   feature = format.read(wkt);
-  if (ratio > 0.15) {
-    feature.style = style_red;
-  } else {
-    feature.style = style_blue;
+
+  if (ratio < 0.05 ){
+    feature.style = style_green;
   }
+  if (ratio > 0.05)  {
+    feature.style = style_yellow;
+  }
+  if (ratio > 0.10){
+    feature.style = style_orange;
+  }
+  if (ratio > 0.15){
+    feature.style = style_red;
+  }
+  if (ratio > 0.20){
+    feature.style = style_red1;
+  }
+  if (ratio > 0.25 ){
+    feature.style = style_red2;
+    }
+
+
   return feature;
 };
 
@@ -30,8 +47,51 @@ style_blue.strokeColor = "blue";
 
 style_blue.fillColor = "blue";
 
+style_green = OpenLayers.Util.extend({}, style_blue);
+
+style_green.strokeColor = "green";
+
+style_green.fillColor = "green";
+
+style_green.fillOpacity ="0.4";
+
 style_red = OpenLayers.Util.extend({}, style_blue);
 
 style_red.strokeColor = "red";
 
 style_red.fillColor = "red";
+
+style_red.fillOpacity = "0.4";
+
+style_red1 = OpenLayers.Util.extend({}, style_blue);
+
+style_red1.strokeColor = "red";
+
+style_red1.fillColor = "red";
+
+style_red1.fillOpacity = "0.7";
+
+
+style_red2 = OpenLayers.Util.extend({}, style_blue);
+
+style_red2.strokeColor = "red";
+
+style_red2.fillColor = "red";
+
+style_red2.fillOpacity = "1";
+
+style_yellow = OpenLayers.Util.extend({}, style_blue);
+
+style_yellow.strokeColor = "yellow";
+
+style_yellow.fillColor = "yellow";
+
+style_yellow.fillOpacity = "0.4";
+
+style_orange = OpenLayers.Util.extend({}, style_blue);
+
+style_orange.strokeColor = "orange";
+
+style_orange.fillColor = "orange";
+
+style_orange.fillOpacity = "0.4";
